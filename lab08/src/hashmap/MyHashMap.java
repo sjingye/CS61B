56cc29594ccd9d3be10acb234e334e293fc377e9
sjingye <sjingye@163.com>
2024-10-24 11:41:29 +0800

    lab08

---
+++ lab08/src/hashmap/MyHashMap.java
@@ -0,0 +1,245 @@
+package hashmap;
+
+import java.util.*;
+
+/**
+ *  A hash table-backed Map implementation.
+ *
+ *  Assumes null keys will never be inserted, and does not resize down upon remove().
+ *  @author YOUR NAME HERE
+ */
+public class MyHashMap<K, V> implements Map61B<K, V> {
+    /**
+     * Protected helper class to store key/value pairs
+     * The protected qualifier allows subclass access
+     */
+    protected class Node {
+        K key;
+        V value;
+
+        Node(K k, V v) {
+            key = k;
+            value = v;
+        }
+    }
+
+    /* Instance Variables */
+    private Collection<Node>[] buckets;
+    // You should probably define some more!
+    private final int initialCapacity;
+    private double loadFactor;
+    int size = 0;
+
+    /** Default initial values */
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+    private static final double DEFAULT_LOAD_FACTOR = 0.75;
+
+    /** Constructors */
+    public MyHashMap() {
+        this(DEFAULT_INITIAL_CAPACITY, DEFAULT_LOAD_FACTOR);
+    }
+
+    public MyHashMap(int initialCapacity) {
+        this(initialCapacity, DEFAULT_LOAD_FACTOR);
+    }
+
+    /**
+     * MyHashMap constructor that creates a backing array of initialCapacity.
+     * The load factor (# items / # buckets) should always be <= loadFactor
+     *
+     * @param initialCapacity initial size of backing array
+     * @param loadFactor maximum load factor
+     */
+    public MyHashMap(int initialCapacity, double loadFactor) {
+        this.initialCapacity = initialCapacity;
+        this.loadFactor = loadFactor;
+        initializeBuckets();
+    }
+
+    private void initializeBuckets() {
+        buckets = new Collection[initialCapacity];
+        for (int i = 0; i < initialCapacity; i++) {
+            buckets[i] = createBucket();
+        }
+    }
+    /**
+     * Returns a data structure to be a hash table bucket
+     *
+     * The only requirements of a hash table bucket are that we can:
+     *  1. Insert items (`add` method)
+     *  2. Remove items (`remove` method)
+     *  3. Iterate through items (`iterator` method)
+     *  Note that that this is referring to the hash table bucket itself,
+     *  not the hash map itself.
+     *
+     * Each of these methods is supported by java.util.Collection,
+     * Most data structures in Java inherit from Collection, so we
+     * can use almost any data structure as our buckets.
+     *
+     * Override this method to use different data structures as
+     * the underlying bucket type
+     *
+     * BE SURE TO CALL THIS FACTORY METHOD INSTEAD OF CREATING YOUR
+     * OWN BUCKET DATA STRUCTURES WITH THE NEW OPERATOR!
+     */
+    protected Collection<Node> createBucket() {
+        // TODO: Fill in this method.
+        return new LinkedList<>();
+    }
+
+    // TODO: Implement the methods of the Map61B Interface below
+    // Your code won't compile until you do so!
+    public void resize() {
+        int capacity = 2 * buckets.length;
+        // rehash
+        Collection<Node>[] newBuckets = new Collection[capacity];
+        for (int i = 0; i < capacity; i++) {
+            newBuckets[i] = createBucket();
+        }
+
+        for (Collection<Node> collection : buckets) {
+            for (Node node : collection) {
+                int newIndex = Math.floorMod(node.key.hashCode(), newBuckets.length);
+                newBuckets[newIndex].add(node);
+            }
+        }
+        buckets = newBuckets;
+    }
+    public void checkLoadFactor() {
+        if (size >= loadFactor * buckets.length) {
+            resize();
+        }
+    }
+    /**
+     * Associates the specified value with the specified key in this map.
+     * If the map already contains the specified key, replaces the key's mapping
+     * with the value specified.
+     *
+     * @param key
+     * @param value
+     */
+    @Override
+    public void put(K key, V value) {
+        checkLoadFactor();
+        int index = getIndex(key);
+        Collection<Node> collection = buckets[index];
+
+        for (Node node : collection) {
+            if (node.key.equals(key)) {
+                node.value = value;
+                return;
+            }
+        }
+
+        collection.add(new Node(key, value));
+        size += 1;
+    }
+
+    /**
+     * Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
+     *
+     * @param key
+     */
+    @Override
+    public V get(K key) {
+        int index = getIndex(key);
+        Collection<Node> collection = buckets[index];
+        for (Node node : collection) {
+            if (node.key.equals(key)) {
+                return node.value;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * get corresponding index in buckets by key.hashCode()
+     * @param key
+     * @return int
+     */
+    public Integer getIndex(K key) {
+        return Math.floorMod(key.hashCode(), buckets.length);
+    }
+    /**
+     * Returns whether this map contains a mapping for the specified key.
+     *
+     * @param key
+     */
+    @Override
+    public boolean containsKey(K key) {
+        int index = getIndex(key);
+        Collection<Node> collection = buckets[index];
+        for (Node node : collection) {
+            if (node.key.equals(key)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Returns the number of key-value mappings in this map.
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Removes every mapping from this map.
+     */
+    @Override
+    public void clear() {
+        initializeBuckets();
+        size = 0;
+    }
+
+    /**
+     * Returns a Set view of the keys contained in this map. Not required for this lab.
+     * If you don't implement this, throw an UnsupportedOperationException.
+     */
+    @Override
+    public Set<K> keySet() {
+        Set<K> set = new HashSet<>();
+        for (Collection<Node> collection : buckets) {
+            for (Node node : collection) {
+                set.add(node.key);
+            }
+        }
+        return set;
+    }
+
+    /**
+     * Removes the mapping for the specified key from this map if present,
+     * or null if there is no such mapping.
+     * Not required for this lab. If you don't implement this, throw an
+     * UnsupportedOperationException.
+     *
+     * @param key
+     */
+    @Override
+    public V remove(K key) {
+        int index = getIndex(key);
+        Collection<Node> collection = buckets[index];
+        for (Node node : collection) {
+            if (node.key.equals(key)) {
+                V value = node.value;
+                collection.remove(node);
+                return value;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<K> iterator() {
+        // throw UnsupportedOperationException();
+        return null;
+    }
+}
